2ee105466e81ac5edc73e0acb486d66138e3e980
민병욱 <woook27@naver.com>
2023-12-17 02:02:23 +0900

    [Feature] 프로젝트 초기 환경 설정 (#1)
    * Chore: add gitignore
    
    * Feat: 프로젝트 초기 설정
    
    * Feat: Local, Production 환경별 설정 분리를 위한 yml 파일 생성

---
+++ src/main/java/kr/ac/konkuk/marsresearchlab/MarsResearchLabApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.konkuk.marsresearchlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsResearchLabApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarsResearchLabApplication.class, args);
+    }
+
+}
