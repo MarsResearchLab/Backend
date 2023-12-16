2c903912171848e3e792417e62b0fc89d1ad4932
woookk <woook27@naver.com>
2023-12-17 01:55:22 +0900

    Feat: 프로젝트 초기 설정

---
+++ src/test/java/kr/ac/konkuk/marsresearchlab/MarsResearchLabApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.konkuk.marsresearchlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsResearchLabApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
