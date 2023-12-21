b467cae129114e72dfb3ae9058c2a0778df5f807
민병욱 <woook27@naver.com>
2023-12-21 22:35:33 +0900

    [Feature] CORS 설정 추가 (#6)
    * Feat: CORS 설정을 위한 WebConfig 구현

---
+++ src/main/java/kr/ac/konkuk/marsresearchlab/global/configs/WebConfig.java
@@ -0,0 +1,15 @@
+package kr.ac.konkuk.marsresearchlab.global.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("*");
+    }
+}
